32fb0a45e59a80f668a7e305b644273660851e44
hafizi1997 <116898680+hafizi1997@users.noreply.github.com>
2025-11-27 14:55:42 +0800

    add

--- src/pages/ProjectPages.jsx
+++ src/pages/ProjectPages.jsx
@@ -2,13 +2,6 @@ import React from "react";
 import ProjectImages from "../components/ProjectImages";
 import Button from "../ui/Button";
 import { useState } from "react";
-import emscc from "../assets/emsc.png";
-import oceztra from "../assets/oceztra.png";
-import anycomp from "../assets/anycomp.png";
-import igaming from "../assets/igaming.png";
-import admin from "../assets/admin.png";
-import ezappBook from "../assets/ezapp_book.png";
-
 export const ProjectsPages = () => {
   const [showAll, setShowAll] = useState(false);
 
@@ -18,7 +11,7 @@ export const ProjectsPages = () => {
       title: "Anycomp",
       description:
         "I developed a product as a freelancer with ST comp Holding Sdn. Bhd.",
-      image: {anycomp},
+      image: "/assets/anycomp-B1-EdhDH.png",
       tags: [
         "NextJs",
         "JavaScript",
@@ -33,15 +26,15 @@ export const ProjectsPages = () => {
       title: "eMSC Sirim Malaysia",
       description:
         "I developed a scheduling module that synchronized with existing internal systems and external APIs, reducing assignment conflicts and improving auditor utilization rates.",
-      image: {emscc},
-      tags: ["React", "JavaScript", "Python", "Java", "MSSQL"],
+      image: "/assets/emsc-eZlkMjXq.png",
+      tags: ["SOAD", "JavaScript", "Python", "Java", "MSSQL"],
       liveUrl: "https://emsc.sirim.my/t/emsc/login",
     },
     {
       id: 3,
       title: "iGaming",
       description: "I working with my friend to build a gaming platform",
-      image: {igaming},
+      image: "/assets/igaming-DDqjk4_k.png",
       tags: ["React", "inertia", "Laravel", "Mysql"],
       liveUrl: "https://assistbot.site/",
     },
@@ -49,7 +42,7 @@ export const ProjectsPages = () => {
       id: 4,
       title: "Admin Dashboard",
       description: "I working with my friend to build a admin panel dashboard",
-      image: {admin},
+      image: "/assets/admin-fdx1Fvma.png",
       tags: ["React", "inertia", "Laravel", "Mysql"],
       liveUrl: "https://dd8draw.com/",
     },
@@ -58,7 +51,7 @@ export const ProjectsPages = () => {
       title: "Oceztra",
       description:
         "I built a task management system that enabled multi-role users to create, edit, and track tasks for both individuals and teams, enhancing task visibility and workflow efficiency.",
-      image: {oceztra},
+      image: "/assets/oceztra-D3bNJR7l.png",
       tags: ["SOAD", "JavaScript", "Python", "Java", "MYSQL"],
       liveUrl: "https://oceztra.com/",
     },
@@ -67,7 +60,7 @@ export const ProjectsPages = () => {
       title: "EzApp",
       description:
         "I developed key features including room management, user booking, user profiles, user registration, and organization dashboard functionality to ensure secure and efficient user interactions.",
-      image: {ezappBook},
+      image: "/assets/ezapp_book-BS__3hHh.png",
       tags: ["React", "Redux", "Node.js", "MYSQL"],
       liveUrl: "https://ezapp.sufia.online/ezapp",
     },
